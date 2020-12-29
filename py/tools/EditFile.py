325f0fb46f5224c875dfe10a504fda8354ffd692
Hank <asdfzxcv7821@gmail.com>
2020-12-29 15:57:28 +0800

    fix inconsistent use of tabs and spaces in indentation

--- py/tools/EditFile.py
+++ py/tools/EditFile.py
@@ -35,17 +35,16 @@ class File():
         gpg = gnupg.GPG(gnupghome=home + '/.gnupg')
         home_fs = open_fs(self.FilePath)
         for path in home_fs.walk.files(filter=['*.gpg']):
-		with home_fs.open(path[1:], 'rb') as f:
-    			status = gpg.decrypt_file(
+            with home_fs.open(path[1:], 'rb') as f:
+                status = gpg.decrypt_file(
 				file=f,
 				passphrase=passphrase,
 				output=self.FilePath + path[:-4]
 			)
-		print("ok: ", status.ok)
-		print("status: ", status.status)
-		print("stderr: ", status.stderr)
-		f.close()
-
+            print("ok: ", status.ok)
+            print("status: ", status.status)
+            print("stderr: ", status.stderr)
+            f.close()
 
 #test case              
 if __name__ == '__main__':
