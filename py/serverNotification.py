2b65639f9c32e9e2a4e12059354f191e3e24bba4
asdfzxcv782 <asdfzxcv7821@gmail.com>
2021-05-03 15:52:25 +0800

    add server userName

--- py/serverNotification.py
+++ py/serverNotification.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import tools.EditFile as file
+import socket
 from requests.auth import HTTPBasicAuth
 
 def sendToServer(status):
     keyDir="/keys"
     FileHandle=file.File(keyDir,"ServerRequestConfig.json")
-    print(json.dumps(status,  indent=4))
+    status["userName"] = socket.gethostname()
     data = FileHandle.ReadFile()
     headers = {'Content-Type': 'application/json'}
     r = requests.post(data['url'], data=json.dumps(status,  indent=4), headers=headers, auth=HTTPBasicAuth(data['name'], data['password']))
